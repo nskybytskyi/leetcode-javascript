01da52db02367c124ad2a23d34decefe0d38918f
Nikita Skybytskyi <n.skybytskyi@gmail.com>
2023-04-17 10:48:30 +0300

    feat: added debounce ts solution

---
+++ debounce/solution.ts
@@ -0,0 +1,17 @@
+type F = (...p: any[]) => any
+
+function debounce(fn: F, t: number): F {
+    let timer: ReturnType<typeof setTimeout>;
+
+    return function (...args) {
+        clearTimeout(timer);
+        timer = setTimeout(fn, t, ...args);
+    }
+};
+
+/**
+ * const log = debounce(console.log, 100);
+ * log('Hello'); // cancelled
+ * log('Hello'); // cancelled
+ * log('Hello'); // Logged at t=100ms
+ */
