5676d5c1a0f275349ccb94b0989e85a72282e284
Nikita Skybytskyi <n.skybytskyi@gmail.com>
2023-04-20 18:29:07 +0300

    feat: added promise pool js solution

---
+++ promise_pool/solution.js
@@ -0,0 +1,21 @@
+/**
+ * @param {Function[]} functions
+ * @param {number} n
+ * @return {Function}
+ */
+var promisePool = async function (functions, n) {
+    const worker = async () => {
+        if (functions.length) {
+            await functions.shift()();
+            await worker();
+        }
+    };
+
+    return Promise.all(Array(n).fill().map(() => worker()));
+};
+
+/**
+ * const sleep = (t) => new Promise(res => setTimeout(res, t));
+ * promisePool([() => sleep(500), () => sleep(400)], 1)
+ *   .then(console.log) // After 900ms
+ */
