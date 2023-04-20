8f90de18f3e50fbbd6d3c8f9814b0b14aa2dc287
Nikita Skybytskyi <n.skybytskyi@gmail.com>
2023-04-20 12:44:16 +0300

    fix(perf): updated snail traversal ts solution - used one for loop over this

--- snail_traversal/solution.ts
+++ snail_traversal/solution.ts
@@ -9,20 +9,17 @@ Array.prototype.snail = function (rowsCount: number, colsCount: number): number[
         return [];
     }
 
-    const array: number[][] = [];
-    for (let row = 0; row < rowsCount; row++) {
-        array.push([]);
+    const array = Array(rowsCount).fill(0).map(() => Array(colsCount).fill(0));
+    let row = 0, col = 0, dir = 1;
 
-        let position = row;
-        for (let col = 0; col < colsCount; col++) {
-            console.log(row, col, position);
-            array[row].push(this[position]);
+    for (const elem of this) {
+        array[row][col] = elem;
 
-            if (col % 2) {
-                position += 2 * row + 1;
-            } else {
-                position += 2 * (rowsCount - row) - 1;
-            }
+        if (0 <= row + dir && row + dir < rowsCount) {
+            row += dir;
+        } else {
+            col++;
+            dir = -dir;
         }
     }
 
