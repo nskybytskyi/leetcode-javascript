e3262f07aaa72168c25517806bdb393d50b301e0
Nikita Skybytskyi <n.skybytskyi@gmail.com>
2023-04-21 12:54:32 +0300

    fix: updated flatten ts solution - removed redundant recursive function

--- flatten_deeply_nested_array/solution.ts
+++ flatten_deeply_nested_array/solution.ts
@@ -1,18 +1,17 @@
 type MultiDimensionalArray = (number | MultiDimensionalArray)[];
 
 const flat = function (arr: MultiDimensionalArray, n: number): MultiDimensionalArray {
+    if (n === 0) {
+        return arr;
+    }
+
     const flattened: MultiDimensionalArray = [];
-    const dfs = (object: number | MultiDimensionalArray, depth: number) => {
-        if (n < depth) {
-            flattened.push(object);
-        } else if (Array.isArray(object)) {
-            for (const value of object) {
-                dfs(value, depth + 1);
-            }
+    for (const value of arr) {
+        if (Array.isArray(value)) {
+            flattened.push(...flat(value, n - 1));
         } else {
-            flattened.push(object);
+            flattened.push(value);
         }
-    };
-    dfs(arr, 0);
+    }
     return flattened;
 };
