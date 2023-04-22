729776f7ab7fe4acf6ea7475f597d688e42bd949
Nikita Skybytskyi <n.skybytskyi@gmail.com>
2023-04-22 13:08:49 +0300

    fix(perf): updated memoize js solution - used json stringify output as a hash

--- memoize/solution.js
+++ memoize/solution.js
@@ -4,10 +4,11 @@
 function memoize(fn) {
     const cache = {};
     return function (...args) {
-        if (!(args in cache)) {
-            cache[args] = fn(...args);
+        const hash = JSON.stringify(args);
+        if (!(hash in cache)) {
+            cache[hash] = fn(...args);
         }
-        return cache[args];
+        return cache[hash];
     }
 }
 
