915fe1c5ca6d84196635e4c3474bacc79c66fa7c
Nikita Skybytskyi <n.skybytskyi@gmail.com>
2023-04-22 13:08:58 +0300

    fix(perf): updated memoize ts solution - used json stringify output as a hash

--- memoize/solution.ts
+++ memoize/solution.ts
@@ -3,10 +3,11 @@ type Fn = (...params: any) => any
 function memoize(fn: Fn): Fn {
     const cache = {};
     return function (...args) {
-        if (!(args in cache)) {
-            cache[args] = fn(...args);
+        const hash = JSON.stringify(args);
+        if (!(hash in cache)) {
+            cache[hash] = fn(...args);
         }
-        return cache[args];
+        return cache[hash];
     }
 }
 
