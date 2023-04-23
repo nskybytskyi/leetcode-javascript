a5d342f43c181345efa9d46ec854e7bcaf39b4b7
Nikita Skybytskyi <n.skybytskyi@gmail.com>
2023-04-23 18:20:48 +0300

    feat: added object class instance js solution

---
+++ check_if_object_instance_of_class/solution.js
@@ -0,0 +1,18 @@
+/**
+ * @param {any} object
+ * @param {any} classFunction
+ * @return {boolean}
+ */
+var checkIfInstanceOf = function (obj, classFunction) {
+    while (obj !== null && obj !== undefined) {
+        if (obj.constructor === classFunction) {
+            return true;
+        }
+        obj = Object.getPrototypeOf(obj);
+    }
+    return false;
+};
+
+/**
+ * checkIfInstanceOf(new Date(), Date); // true
+ */
