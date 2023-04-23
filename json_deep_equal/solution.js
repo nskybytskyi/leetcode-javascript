522d0e8f09bd927ed21e4ded7a1f338a7d7ee263
Nikita Skybytskyi <n.skybytskyi@gmail.com>
2023-04-23 17:59:54 +0300

    feat: added json deep equal js solution

---
+++ json_deep_equal/solution.js
@@ -0,0 +1,24 @@
+/**
+ * @param {any} o1
+ * @param {any} o2
+ * @return {boolean}
+ */
+var areDeeplyEqual = function (o1, o2) {
+    if (typeof o1 !== typeof o2 || Array.isArray(o1) !== Array.isArray(o2)) {
+        return false;
+    }
+    if (typeof o1 === "boolean" || typeof o1 === "number" || typeof o1 === "string") {
+        return o1 === o2;
+    }
+    for (const key in o1) {
+        if (!(key in o2) || !areDeeplyEqual(o1[key], o2[key])) {
+            return false;
+        }
+    }
+    for (const key in o2) {
+        if (!(key in o1)) {
+            return false;
+        }
+    }
+    return true;
+};
