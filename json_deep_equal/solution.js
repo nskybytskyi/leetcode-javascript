b02a3741ced33b146888f6046de748270b556cce
Nikita Skybytskyi <n.skybytskyi@gmail.com>
2023-04-23 18:05:03 +0300

    fix: updated json deep equal js solution - simplified some checks

--- json_deep_equal/solution.js
+++ json_deep_equal/solution.js
@@ -4,19 +4,16 @@
  * @return {boolean}
  */
 var areDeeplyEqual = function (o1, o2) {
-    if (typeof o1 !== typeof o2 || Array.isArray(o1) !== Array.isArray(o2)) {
+    if (o1 === o2) {
+        return true;
+    }
+    if (typeof o1 !== "object" || typeof o2 !== "object" ||
+        Array.isArray(o1) !== Array.isArray(o2) ||
+        Object.keys(o1).length !== Object.keys(o2).length) {
         return false;
     }
-    if (typeof o1 === "boolean" || typeof o1 === "number" || typeof o1 === "string") {
-        return o1 === o2;
-    }
     for (const key in o1) {
-        if (!(key in o2) || !areDeeplyEqual(o1[key], o2[key])) {
-            return false;
-        }
-    }
-    for (const key in o2) {
-        if (!(key in o1)) {
+        if (!areDeeplyEqual(o1[key], o2[key])) {
             return false;
         }
     }
